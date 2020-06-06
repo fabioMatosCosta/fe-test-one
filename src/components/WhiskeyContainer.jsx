8f1122f6a94ac95b491eed9443e8cc6b8f5e5602
Fábio Costa <fabiodanielmc@gmail.com>
2020-06-06 18:16:13 +0200

    background and logic for the top line

--- src/components/WhiskeyContainer.jsx
+++ src/components/WhiskeyContainer.jsx
@@ -16,17 +16,17 @@ class WhiskeyContainer extends Component {
     getStyle(region){
         let style = {};
         switch(region){
-            case 'island':
+            case 'islands':
                 style = {'backgroundImage': 'linear-gradient(to right, #D71E44 , #9E1B34)'};
                 break;
-            case'islay':
+            case 'islay':
                 style = {'backgroundImage': 'linear-gradient(to right, #A5238E , #6E2671)'};
                 break;
             case 'highlands':
                 style={'backgroundImage': 'linear-gradient(to right, #563494 , #322773)'};
                 break;
             case 'lowlands':
-                style={'backgroundImage': 'linear-gradient(to right, #O663AE , #174272)'};
+                style={'backgroundImage': 'linear-gradient(to right, #0663AE , #174272)'};
                 break;
             case 'speyside':
                 style={'backgroundImage': 'linear-gradient(to right, #1AB467 , #007B46)'};
@@ -40,11 +40,39 @@ class WhiskeyContainer extends Component {
         return style
     }
 
+    getColor(region){
+        let style = {};
+        switch(region){
+            case 'islands':
+                style = {'border': '2px solid #9E1B34'};
+                break;
+            case'islay':
+                style = {'border': '2px solid #6E2671'};
+                break;
+            case 'highlands':
+                style={'border': '2px solid #322773'};
+                break;
+            case 'lowlands':
+                style={'border': '2px solid #174272'};
+                break;
+            case 'speyside':
+                style={'border': '2px solid #007B46'};
+                break;
+            case 'campbeltown':
+                style={'border': '2px solid #D29D2A'};
+                break;
+            default:
+                style={}
+        }
+        return style
+    }
+
     render() {
         return (
-            <div className = "container column is-one-third">
+            <div className = "container column is-one-third wh-container">
                 <Link to= {`${this.props.uri}`}>
                     <div className ="whiskeyContainer">
+                        <span style = {this.getColor(this.props.region)} className="line"></span>
                         <div className ="wh-info">
                             <div className ="wh-text">
                                 <h3 className ="wh-name">{_.capitalize(this.props.name)}</h3>
@@ -52,14 +80,13 @@ class WhiskeyContainer extends Component {
                                 <h3 className = "wh-name">${this.props.cost}</h3>
                             </div>
                             <div className ="notes-container">
-                            <div style = {this.getStyle(this.props.region)}className ="wh-notes">
-                                {this.props.notes.map((note, index)=>{
-                                    return(
-                                        <p className="note" key = {index}>{_.capitalize(note)}</p>
+                                <div style = {this.getStyle(this.props.region)} className ="wh-notes">
+                                    {this.props.notes.map((note, index)=>{
+                                        return(
+                                            <p className="note" key = {index}>{_.capitalize(note)}</p>
+                                        )}
                                     )}
-                                )}
-                            </div>
-
+                                </div>
                             </div>
                         </div>
                         <div className ="bottle">
